1351fb21b71462c84f113a48738c96e12be786c7
johngiorgi <johnmgiorgi@gmail.com>
2019-12-19 11:05:20 -0500

    :recycle: Refactor to one module, pool is optional

--- modules/lama.py
+++ modules/lama.py
@@ -1,3 +1,4 @@
+import math
 from typing import Callable
 
 import torch
@@ -34,7 +35,6 @@ class LAMA(Module):
     normalize : ``bool``, optional (default: ``True``)
         If true, we normalize the computed similarities with a softmax, to return a probability
         distribution for each attention head.  If false, this is just computing a similarity score.
-    bias : TODO.
     """
 
     def __init__(
@@ -43,46 +43,50 @@ class LAMA(Module):
         input_dim: int,
         activation: Callable = torch.tanh,
         normalize: bool = True,
-        # TODO (John): How to get max_len without asking for it explicitly?
-        bias: bool = False
     ) -> None:
         super().__init__()
         self._activation = (lambda x: x) if activation is None else activation
         self._normalize = normalize
         self._p = Parameter(torch.Tensor(input_dim, num_heads))
         self._q = Parameter(torch.Tensor(input_dim, num_heads))
-        self._c = Parameter(torch.Tensor(input_dim, 1))
 
-        if bias:
-            # TODO (John): out_features is equal to max_sequence_length. How to get this without
-            # asking for it?
-            self._bias = Parameter(torch.Tensor(out_features))
-        else:
-            self.register_parameter('_bias', None)
         self.reset_parameters()
 
     def reset_parameters(self):
-        torch.nn.init.xavier_uniform_(self._p)
-        torch.nn.init.xavier_uniform_(self._q)
-        torch.nn.init.xavier_uniform_(self._c)
-        if self._bias is not None:
-            self._bias.data.fill_(0)
+        torch.nn.init.kaiming_uniform_(self._p, a=math.sqrt(5))
+        torch.nn.init.kaiming_uniform_(self._q, a=math.sqrt(5))
 
-    def forward(self, inputs: torch.Tensor, mask: torch.Tensor = None) -> torch.Tensor:
+    def forward(self, inputs: torch.Tensor, mask: torch.Tensor = None, pool: bool = False) -> torch.Tensor:
         similarities = self._forward_internal(inputs, mask)
 
         if self._normalize:
             if mask is not None:
-                # The 2 dimension (num_heads) will be broadcasted
+                # The 2nd dimension (num_heads) will be broadcasted
                 similarities = similarities.masked_fill(mask.unsqueeze(1) == 0, -1e9)
-            return F.softmax(similarities, dim=-1)
-        else:
-            return similarities
+            similarities = F.softmax(similarities, dim=-1)
+
+        if pool:
+            sequence_embedding_matrix = similarities @ inputs
+            return sequence_embedding_matrix.view(inputs.size(0), -1)
+
+        # If pool: (batch_size, num_heads * input_dim)
+        # Else:    (batch_size, num_heads, max_seq_len)
+        return similarities
 
     def _forward_internal(self, inputs: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
-        # TODO (John): Missing L2 norm
-        # See Eq. 3.13 of https://arxiv.org/abs/1912.00835
-        q_t_h_t = self._q.t() @ inputs.transpose(1, 2)
-        p_t_c_g = self._p.t() @ self._c.expand(-1, inputs.size(1))
+        # The global context vector for each input is the mean of the word embeddings
+        if mask is not None:
+            c = (torch.sum(inputs * mask.unsqueeze(-1), dim=1) /
+                 torch.clamp(torch.sum(mask, dim=1, keepdims=True), min=1e-9))
+        else:
+            c = torch.mean(inputs, dim=1)
 
-        return self._activation(p_t_c_g * q_t_h_t)
+        # See Eq. 3.13 of https://arxiv.org/abs/1912.00835
+        q_h = self._q.t() @ inputs.transpose(1, 2)
+        p_c_g = self._p.t() @ c.unsqueeze(-1)
+
+        alignment = self._activation(p_c_g * q_h)
+        alignment /= torch.norm(alignment, dim=1, keepdim=True)  # l2 normalization
+
+        # (batch_size, num_heads, max_seq_len)
+        return alignment
